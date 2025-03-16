58206b0e2c4ab56b262279d378631206946c1063
rohmanalka <rohmanalka06@gmail.com>
2025-03-16 17:25:54 +0700

    JS4 Prak 2.1 Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,15 +32,18 @@ class UserController extends Controller
         // ];
         // UserModel::insert($data); // Tambahkan data ke table m_user
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make(12345)
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make(12345)
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
+
         return view('user', ['data' => $user]);
     }
 }
