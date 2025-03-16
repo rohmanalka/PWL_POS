0b3e2bb029b3159cbd91c7fbf4339ced349ddc7a
rohmanalka <rohmanalka06@gmail.com>
2025-03-16 17:31:21 +0700

    JS4 Prak 2.2 Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -40,10 +40,7 @@ class UserController extends Controller
         // ];
         // UserModel::create($data);
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function() {
-            abort(404);
-        });
-
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
