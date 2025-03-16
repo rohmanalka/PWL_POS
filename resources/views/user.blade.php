2fba46bbef768a37d26b8ae43a0148016e5ef8a6
rohmanalka <rohmanalka06@gmail.com>
2025-03-16 21:25:15 +0700

    JS4 Prak 2.7 Relationships

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -14,6 +14,9 @@
             <td>Username</td>
             <td>Nama</td>
             <td>ID Level Pengguna</td>
+            <td>Kode Level</td>
+            <td>Nama Level</td>
+            <td>Aksi</td>
         </tr>
         @foreach ($data as $d)
         <tr>
@@ -21,6 +24,8 @@
             <td>{{$d->username}}</td>
             <td>{{$d->nama}}</td>
             <td>{{$d->level_id}}</td>
+            <td>{{$d->level->level_kode}}</td>
+            <td>{{$d->level->level_nama}}</td>
             <td><a href="/user/ubah/{{$d->user_id}}">Ubah</a> | <a href="/user/hapus/{{$d->user_id}}">Hapus</a></td>
         </tr>
         @endforeach
