83f8e6f893ebf37c3a4f94833ddd34daf7350528
rohmanalka <rohmanalka06@gmail.com>
2025-03-22 17:34:38 +0700

    JS5 TUGAS UPDATE

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -12,7 +12,7 @@ class BarangModel extends Model
 
     protected $table = "m_barang";
     protected $primaryKey = "barang_id";
-    protected $fillable = ['kategori_id, barang_kode', 'barang_nama', 'harga_beli', 'harga_jual'];
+    protected $fillable = ['kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual'];
 
     public function kategori(): BelongsTo
     {
