36e748f385b1b8343581ba90f1a92c456cde37fe
rohmanalka <rohmanalka06@gmail.com>
2025-03-22 23:04:58 +0700

    JS6 PRAK3 Modal Ajax Hapus Data (Data User)

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::group(['prefix' => 'user'], function () {
     Route::post('/list', [UserController::class, 'list']);
     Route::get('/create', [UserController::class, 'create']);
     Route::post('/', [UserController::class, 'store']);
-    // Ajax
+    // Ajax Create
     Route::get('/create_ajax', [UserController::class, 'create_ajax']);
     Route::post('/ajax', [UserController::class, 'store_ajax']);
 
@@ -39,7 +39,9 @@ Route::group(['prefix' => 'user'], function () {
     // Ajax Update
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);
-    
+    // Ajax Delete
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']);
     Route::delete('/{id}', [UserController::class, 'destroy']);
 });
 
