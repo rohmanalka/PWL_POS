449e3854232473d8b9cc632142272414117b37dd
rohmanalka <rohmanalka06@gmail.com>
2025-03-30 04:53:59 +0700

    JS7 TUGAS4 Form Register

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,8 @@ Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postLogin']);
 Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/register', [AuthController::class, 'postRegister']);
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/', [WelcomeController::class, 'index']);
