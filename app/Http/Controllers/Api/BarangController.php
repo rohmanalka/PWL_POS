15f082e516cceb804f3c1137435a531c4316f656
rohmanalka <rohmanalka06@gmail.com>
2025-05-03 23:19:37 +0700

    JS10 TUGAS Implementasi CRUD API table m_barang

---
+++ app/Http/Controllers/Api/BarangController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\BarangModel;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class BarangController extends Controller
+{
+    public function index()
+    {
+        return BarangModel::all();
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'kategori_id' => 'required',
+            'barang_kode' => 'required',
+            'barang_nama' => 'required',
+            'harga_beli' => 'required',
+            'harga_jual' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $barang = BarangModel::create([
+            'kategori_id' => $request->kategori_id,
+            'barang_kode' => $request->barang_kode,
+            'barang_nama' => $request->barang_nama,
+            'harga_beli' => $request->harga_beli,
+            'harga_jual' => $request->harga_jual
+        ]);
+
+        if ($barang) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Data berhasil disimpan',
+                'barang' => $barang
+            ], 201);
+        }
+
+        return response()->json([
+            'success' => false,
+        ], 409);
+    }
+
+    public function show(BarangModel $barang)
+    {
+        return $barang;
+    }
+
+    public function update(Request $request, BarangModel $barang)
+    {
+        $barang->update($request->all());
+        return $barang;
+    }
+
+    public function destroy(BarangModel $barang)
+    {
+        $barang->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data berhasil dihapus'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\LevelController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\BarangController;
 use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\KategoriController;
 use App\Http\Controllers\Api\RegisterController;
@@ -44,3 +45,9 @@ Route::post('kategoris', [KategoriController::class, 'store']);
 Route::get('kategoris/{kategori}', [KategoriController::class, 'show']);
 Route::put('kategoris/{kategori}', [KategoriController::class, 'update']);
 Route::delete('kategoris/{kategori}', [KategoriController::class, 'destroy']);
+
+Route::get('barangs', [BarangController::class, 'index']);
+Route::post('barangs', [BarangController::class, 'store']);
+Route::get('barangs/{barang}', [BarangController::class, 'show']);
+Route::put('barangs/{barang}', [BarangController::class, 'update']);
+Route::delete('barangs/{barang}', [BarangController::class, 'destroy']);
